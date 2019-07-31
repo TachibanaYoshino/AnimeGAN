a38c1bca210794fcb4fe3d9dfb32451b1e895fcf
CC <36946777+TachibanaYoshino@users.noreply.github.com>
2019-07-31 14:11:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# from cartoonGAN import cartoonGAN
 from AnimeGAN import AnimeGAN
 import argparse
 from utils import *
@@ -26,7 +25,7 @@ def parse_args():
 
     parser.add_argument('--g_adv_weight', type=float, default=300.0, help='Weight about GAN')
     parser.add_argument('--d_adv_weight', type=float, default=300.0, help='Weight about GAN')
-    parser.add_argument('--con_weight', type=float, default=1.5, help='Weight about VGG19')
+    parser.add_argument('--con_weight', type=float, default=1.5, help='Weight about VGG19') # 1.1 for Shinkai
     # ------ the follow weight used in AnimeGAN
     parser.add_argument('--sty_weight', type=float, default=3.0, help='Weight about style')
     parser.add_argument('--color_weight', type=float, default=10.0, help='Weight about color')
